1be5c64e9b536fe0544d51713c0f7a05a4c4d11f
jhomer0306 <jhomer0306@gmail.com>
2024-11-07 17:53:12 -0800

    Contains a setup test for connectivity to Urban Routes server and eight placeholder tests which will later be used to test eight areas of functionality in Urban Routes.  These will be elaborated in S8.

--- data.py
+++ data.py
@@ -1,1 +1,15 @@
 # File with constants
+
+# URL for Urban Routes
+URBAN_ROUTES_URL = 'https://cnt-f1469b63-ca5b-448d-87b6-b31e8123358c.containerhub.tripleten-services.com'
+
+# Tuple containing tariff options
+TARIFFS = ('Business', 'Sleepy', 'Holiday', 'Talking', 'Supportive', 'Glossy')
+
+# Constants for test data
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,67 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+
+# setup_class is used to establish a single connection to Urban Routes server
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+            exit()
+# URL is specified in data.py.
+# If the Urban Routes server is reachable, print "Connected...".  Otherwise, print "cannot connect..."
+# Check server connection before proceeding with other tests.  Other tests will fail if server has expired.
+
+    def test_set_route(self):
+        # placeholder for setting a route; will be expanded in later steps
+        # Add in S8
+        print("Function created for setting route")
+        pass
+
+    def test_select_route(self):
+        # placeholder for selecting a route; will be expanded in later steps
+        # Add in S8
+        print("Function created for selecting route")
+        pass
+
+    def test_fill_phone_number(self):
+        # placeholder for filling in a phone number; will be expanded in later steps
+        # Add in S8
+        print("Function created for filling phone number")
+        pass
+
+    def test_fill_card(self):
+        # placeholder for filling in card details; will be expanded in later steps
+        # Add in S8
+        print("Function created for filling card details")
+        pass
+
+    def test_comment_for_driver(self):
+        # placeholder for leaving driver a comment; will be expanded in later steps
+        # Add in S8
+        print("Function created for leaving driver a comment")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # placeholder for ordering blanket/handkerchief; will be expanded in later steps
+        # Add in S8
+        print("Function created for ordering blanket and handkerchiefs")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # placeholder for ordering two ice creams; will be expanded in later steps
+        # Add in S8
+        for test_order in range(2):
+        # Loop to simulate ordering two ice creams
+        #Add in S8
+            print(f"Function created for ordering ice cream #{test_order +1}")
+        # Prints separately for ice cream #1 and ice cream #2
+            pass
+
+    def test_car_search_model_appears(self):
+        # placeholder for car search model; will be expanded in later steps
+        # Add in S8
+        print("Function created for car search model appears")
+        pass
